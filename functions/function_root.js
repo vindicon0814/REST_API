1dd583f3b72d5dd3075b69ba086b5cb9560a3405
vindicon0814 <vindicon0814@gmail.com>
2021-02-10 18:04:46 +0800

    modify functions/function_root.js

--- functions/function_root.js
+++ functions/function_root.js
@@ -1,4 +1,4 @@
 app.get("/PING_API", (req, res) => {
 	console.log("Responding to root route")
-	res.send("Hello. I am gROOT and Connection is Established...")	
+	res.send("Hello. I am gROOT and Connection is Established....")	
 })
